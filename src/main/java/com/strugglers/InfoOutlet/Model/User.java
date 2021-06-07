5638bd18448331b93d4e8c7dee07072bc783f872
hakulakhe <hakulakhe@gmail.com>
2021-06-07 08:11:45 +0545

    Database connected and user table created

---
+++ src/main/java/com/strugglers/InfoOutlet/Model/User.java
@@ -0,0 +1,89 @@
+package com.strugglers.InfoOutlet.Model;
+
+import javax.persistence.*;
+
+@Entity     //Maps objects with database
+@Table(name = "user")       //Specify which table to map it with
+public class User
+{
+    @Id     //For primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     //user id is auto incremented sequentially not randomly like in AUTO
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    @Column(name = "my_last_name")      //Just like AS in the mysql queires
+    private String lastName;
+    private String address;
+
+    public User()
+    {
+    }
+
+    public User(int id, String username, String password, String email, String firstName, String lastName, String address) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
